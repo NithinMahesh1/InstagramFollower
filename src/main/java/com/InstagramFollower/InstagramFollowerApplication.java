2ce4d7ca35dfd840612a989034373ecd1ed93b3e
Nithin Mahesh <nmahesh@umass.edu>
2022-05-14 11:48:29 -0400

    Adding the intilization files with Spring Intializr

---
+++ src/main/java/com/InstagramFollower/InstagramFollowerApplication.java
@@ -0,0 +1,13 @@
+package com.InstagramFollower;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InstagramFollowerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InstagramFollowerApplication.class, args);
+	}
+
+}
